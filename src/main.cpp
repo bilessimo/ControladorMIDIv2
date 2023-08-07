c6b69e9aa98bba7f5feb25678e2373a7769a49af
Gustavo Biléssimo <gbilessimo@gmail.com>
2023-08-07 11:10:12 -0300

    Limpeza e organização do código.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,27 @@
+/*
+CONTROLADOR MIDI
+Cadeia: Arduino Nano -> Hairless -> loopMidi -> Ableton
+Potenciômetros: 1
+Botões: 1 (Push Button)
+LEDs: 2
+*/
+
 #include <Arduino.h>
 #include <MIDI.h>
 
 MIDI_CREATE_DEFAULT_INSTANCE();
 
-int pot_ValorAtual = 0;
-int pot_ValorAnterior = 0;
-int pot_LimiteValor = 1;
+int pot_valor_atual = 0;      // Valor do potenciômetro no frame atual.
+int pot_valor_anterior = 0;   // Último valor do potenciômetro que foi aceito.
+int pot_valor_limite = 1;     // Valor que o potenciômetro precisa mudar para que o valor seja aceito.
+long pot_tempo = 0;           
+long pot_tempo_limite = 10;   // Valor mínimo (em milisegundos) para o debouncing do potenciômetro.
 
-int PC_pot_ValorAtual = 0;
+bool bot_estado = false;
+bool led_estado = false;
 
+// Resolve as mensagens que são recebidas.
+// Por enquanto é um placeholder que só acende o led interno do Arduíno quando o valor recebido é maior do que um limite;
 void HandleIncomeCC(byte channel, byte number, byte value) {
   if (number == 11)
   {
@@ -20,31 +33,57 @@ void HandleIncomeCC(byte channel, byte number, byte value) {
     {
       digitalWrite(LED_BUILTIN, false);
     } 
-  }
-  
+  }  
 }
 
-void POT1() {
-  pot_ValorAtual = map(analogRead(A0), 0, 1023, 0, 126);
-  int diferenca = abs(pot_ValorAtual - pot_ValorAnterior);
-
-  if (diferenca > pot_LimiteValor)
+// Lê um botão físico ligado ao Arduino.
+// Por enquanto é um placeholder que só acende um led quando o botão é apertado, para fins de debug.
+void BOT1() {
+  if (digitalRead(4) != bot_estado)
   {
-    MIDI.sendControlChange(11, pot_ValorAtual, 1);
-    pot_ValorAnterior = pot_ValorAtual;
-  }  
+    led_estado = !led_estado;
+  }
+  
+  if (led_estado)
+  {
+    digitalWrite(9, HIGH);
+  } else {
+    digitalWrite(9, LOW);
+  }
+  
+  // TODO: Enviar a mensagem de que o botão foi apertado.
+}
+
+// Resolve a leitura do potenciômetro e envia a mensagem de CC.
+// TODO: Fazer resolver um array de potenciômetros.
+void POT1() {
+  pot_valor_atual = map(analogRead(A0), 0, 1023, 0, 127);     // Lê o potenciômetro e mapeia o valor em 7 bits.
+  int diferenca = abs(pot_valor_atual - pot_valor_anterior);  // Determina a diferença entre o valor atual e a última leitura válida.
+
+  if (millis() - pot_tempo > pot_tempo_limite)                // Debouncing
+  {
+    if (diferenca > pot_valor_limite)
+    {
+      MIDI.sendControlChange(11, pot_valor_atual, 1);         // TODO: Tirar os valores absolutos.
+      pot_valor_anterior = pot_valor_atual;
+      pot_tempo = millis();
+    }
+  }
 }
 
 void setup() {
   
-  pinMode(LED_BUILTIN, OUTPUT);   
+  pinMode(LED_BUILTIN, OUTPUT);     // Led interno.
+  pinMode(9, OUTPUT);               // Led externo.
+  pinMode(4, INPUT);                // Potenciômetro.
 
-  MIDI.setHandleControlChange(HandleIncomeCC);  
-  MIDI.begin(1);
+  MIDI.setHandleControlChange(HandleIncomeCC);  // Define a função que será chamada quando receber uma mensagem CC.
+  MIDI.begin(1);                                // ??? Não tenho certeza, mas acho que a ordem em que a comunicação MIDI e a Serial são iniciadas tem influência.
   Serial.begin(115200);
 }
 
 void loop() {
   MIDI.read(1);
   POT1();  
+  BOT1();
 }
